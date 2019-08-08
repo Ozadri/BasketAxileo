3c9bc558c3e2e3d7548f71a4a80826f35360cfc1
BasketAxileo <meltzer.adrien@gmail.com>
2019-08-08 18:08:28 +0200

    j1

---
+++ Assets/Scripts/Detection.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using Vuforia;
+
+public class Detection : MonoBehaviour, ITrackableEventHandler
+{
+    public GameObject ballon;
+    public float swipeDistanceThreshold = 50;
+    public Text debugText;
+
+    private bool ballonPretATirer = false;
+    private GameObject ballonDeTir;
+    private TrackableBehaviour mTrackableBehaviour;
+    private Touch touch;
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            
+            if (!ballonPretATirer)
+            {
+                ballonDeTir = Instantiate(ballon);
+                ballonPretATirer = !ballonPretATirer;
+            } else
+            {
+                ballonDeTir.SetActive(true);
+            }
+            
+        }
+        else
+        {
+            //debugText.text = "NON";
+            if (ballonPretATirer)
+            {
+                ballonDeTir.SetActive(false);
+            }
+            
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+        touch = new Touch();
+
+        
+
+    }
+
+    private Vector2 startposition;
+    private Vector2 endposition;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.touches[0];
+            
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startposition = touch.position;
+                    break;
+                case TouchPhase.Ended:
+                    endposition = touch.position;
+                    
+                    if (analyseGesture(startposition, endposition))
+                    {
+                        Tir();
+                    }
+                    else
+                    {
+                        if (ballonPretATirer == false)
+                        {
+                            ballonDeTir = Instantiate(ballon);
+                            ballonPretATirer = !ballonPretATirer;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startposition = new Vector2(Screen.width/2,0);
+            endposition = new Vector2(Screen.width / 2, Screen.height / 2);
+
+            Tir();
+        }
+    }
+
+    private bool analyseGesture(Vector2 start, Vector2 end)
+    {
+        if (Vector2.Distance(start, end) > swipeDistanceThreshold)
+            return true;
+        
+        return false;
+    }
+
+    private void Tir()
+    {
+        Debug.Log("TIR");
+        Rigidbody rigBallon = ballonDeTir.GetComponent<Rigidbody>();
+        rigBallon.useGravity = true;
+
+        float x = (endposition.x - startposition.x) / (Screen.width / 4);
+        float y = (endposition.y - startposition.y) / (Screen.height / 7);
+
+        //debugText.text = "X : " + x + " Y : " + y;
+
+        if (y > 2.7f)
+        {
+            y = 2.7f;
+        }
+
+        rigBallon.AddForce(new Vector3(x, y, 1.5f), ForceMode.Impulse);
+
+        ballonDeTir = null;
+        ballonPretATirer = !ballonPretATirer;
+    }
+
+}
---
+++ Assets/Scripts/gameplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class gameplay : MonoBehaviour
+{
+    public Text score;
+    private int nbScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score.text = "Panier(s) : " + nbScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        nbScore = nbScore + 1;
+        score.text = "Panier(s) : " + nbScore;
+
+        if (nbScore >= 10)
+        {
+            score.text = "TU AS GAGNE ! VIENT NOUS VOIR !";
+        }
+    }
+}
