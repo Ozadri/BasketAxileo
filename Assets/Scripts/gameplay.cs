0f61950d0015df1321fe791a2623101c80e317f6
BasketAxileo <meltzer.adrien@gmail.com>
2019-08-09 11:20:54 +0200

    Finish

--- Assets/Scripts/gameplay.cs
+++ Assets/Scripts/gameplay.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class gameplay : MonoBehaviour
 {
     public Text score;
+    public List<ParticleSystem> fireworks;
     private int nbScore = 0;
     // Start is called before the first frame update
     void Start()
@@ -19,12 +20,19 @@ public class gameplay : MonoBehaviour
         
     }
 
+    private void StartFireWorks()
+    {
+        foreach (ParticleSystem particleSystem in fireworks)
+        {
+            particleSystem.Play();
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        //Destroy(other.gameObject);
-        nbScore = nbScore + 1;
+        nbScore++;
         score.text = "Panier(s) : " + nbScore;
-
+        StartFireWorks();
         if (nbScore >= 10)
         {
             score.text = "TU AS GAGNE ! VIENT NOUS VOIR !";
